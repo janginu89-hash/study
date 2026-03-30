444c9305aece9bd005bf656cc5dba3bf2d9bc3be
장인우 <janginu89@gmail.com>
2026-03-30 18:01:21 +0900

    파일

--- 1.py
+++ 1.py
@@ -1,7 +1,15 @@
 n,m=map(int,input().split())
-b=[1,2,3,4,5]
 r='안녕'
+b=[]
+a=''
+for i in range(1,n+1):
+    b+=str(i)
+
 for a in range(m):
-    i,j=input().split()
-    y=i
-    b.replace()
+    i,j=map(int,input().split())
+    y=b[i-1]
+    z=b[j-1]
+    b[i-1]=z
+    b[j-1]=y
+for i in b:
+    str(a)+=str(i)+' '
